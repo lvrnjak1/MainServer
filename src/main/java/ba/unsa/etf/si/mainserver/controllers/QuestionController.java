063a307861d4328fd62938b96fec5aab7b602b84
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2020-03-25 23:05:34 +0100

    added protection to pr endpoints

--- src/main/java/ba/unsa/etf/si/mainserver/controllers/QuestionController.java
+++ src/main/java/ba/unsa/etf/si/mainserver/controllers/QuestionController.java
@@ -6,10 +6,12 @@ import ba.unsa.etf.si.mainserver.models.pr.Question;
 import ba.unsa.etf.si.mainserver.models.pr.QuestionAuthor;
 import ba.unsa.etf.si.mainserver.requests.pr.AnswerRequest;
 import ba.unsa.etf.si.mainserver.requests.pr.QuestionRequest;
+import ba.unsa.etf.si.mainserver.responses.ApiResponse;
 import ba.unsa.etf.si.mainserver.responses.pr.QuestionResponse;
 import ba.unsa.etf.si.mainserver.services.pr.AnswerService;
 import ba.unsa.etf.si.mainserver.services.pr.QuestionService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
@@ -48,6 +50,7 @@ public class QuestionController {
     }
 
     @GetMapping("/answered")
+    @Secured({"ROLE_PRW", "ROLE_ADMIN"})
     public List<QuestionResponse> getAllAnsweredQuestions(){
         return questionService.getAll().stream().
                 filter(question -> question.getAnswer() != null)
@@ -56,6 +59,7 @@ public class QuestionController {
     }
 
     @GetMapping("/unanswered")
+    @Secured({"ROLE_PRW", "ROLE_ADMIN"})
     public List<QuestionResponse> getAllUnansweredQuestions(){
         return questionService.getAll().stream().
                 filter(question -> question.getAnswer() == null)
@@ -64,15 +68,17 @@ public class QuestionController {
     }
 
     @DeleteMapping("/{questionId}")
+    @Secured({"ROLE_PRW", "ROLE_ADMIN"})
     public ResponseEntity<?> deleteQuestion(@PathVariable("questionId") Long questionId){
         return questionService.findQuestionById(questionId)
                 .map(question -> {
                     questionService.delete(question);
-                    return ResponseEntity.ok().build();
+                    return ResponseEntity.ok(new ApiResponse("Question successfully deleted", 200));
                 }).orElseThrow(() -> new AppException("Question with id " + questionId + " doesn't exist"));
     }
 
     @PostMapping("/{questionId}/answer")
+    @Secured({"ROLE_PRW", "ROLE_ADMIN"})
     public QuestionResponse saveAnswerToQuestion(@RequestBody AnswerRequest answerRequest, @PathVariable("questionId") Long questionId){
         return questionService.findQuestionById(questionId).map(question -> {
             Answer answer = new Answer(answerRequest.getText());
@@ -81,13 +87,14 @@ public class QuestionController {
     }
 
     @DeleteMapping("/{questionId}/answer")
+    @Secured({"ROLE_PRW", "ROLE_ADMIN"})
     public ResponseEntity<?> deleteAnswer(@PathVariable("questionId") Long questionId){
 
         return answerService.findByQuestionId(questionId).map(answer -> questionService.findQuestionById(questionId).map(question -> {
             question.setAnswer(null);
             questionService.save(question);
             answerService.delete(answer);
-            return ResponseEntity.ok().build();
+            return ResponseEntity.ok(new ApiResponse("Answer successfully deleted", 200));
         }).orElseThrow(()->new AppException("Question with id " + questionId + " doesn't exist"))).orElseThrow(() -> new AppException("Question with id " + questionId + " doesn't have an answer"));
     }
 
---
+++ src/main/java/ba/unsa/etf/si/mainserver/responses/ApiResponse.java
@@ -0,0 +1,37 @@
+package ba.unsa.etf.si.mainserver.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+    private String message;
+    private int statusCode;
+
+//    public ApiResponse(String message, int statusCode) {
+//        this.message = message;
+//        this.statusCode = statusCode;
+//    }
+//
+//    public ApiResponse() {
+//    }
+//
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//    public void setMessage(String message) {
+//        this.message = message;
+//    }
+//
+//    public int getStatusCode() {
+//        return statusCode;
+//    }
+//
+//    public void setStatusCode(int statusCode) {
+//        this.statusCode = statusCode;
+//    }
+}

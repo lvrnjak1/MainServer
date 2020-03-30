94d522425a4c28e4978dee5b6c2cd7246c839534
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2020-03-30 15:18:10 +0200

    changed roleId to id

--- src/main/java/ba/unsa/etf/si/mainserver/responses/auth/RoleResponse.java
+++ src/main/java/ba/unsa/etf/si/mainserver/responses/auth/RoleResponse.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RoleResponse {
-    private Long roleId;
+    private Long id;
     private String rolename;
 }

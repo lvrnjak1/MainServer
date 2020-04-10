9db01b46c99860e3197be0d90f936cab62424277
Tarik Sijercic <tsijercic1@etf.unsa.ba>
2020-04-10 20:56:09 +0200

    added all action names for logging

--- src/main/java/ba/unsa/etf/si/mainserver/configurations/Actions.java
+++ src/main/java/ba/unsa/etf/si/mainserver/configurations/Actions.java
@@ -4,4 +4,53 @@ public class Actions {
     public static final String ADMIN_PASSWORD_CHANGE_ACTION_NAME = "admin_password_change";
     public static final String LOGIN_ACTION_NAME = "login";
     public static final String PASSWORD_CHANGE_ACTION_NAME = "password_change";
+    public static final String ADMIN_REGISTER_USER_ACTION_NAME = "admin_register_user";
+    public static final String REGISTER_USER_ACTION_NAME = "register_user";
+
+    public static final String ADMIN_CREATE_BUSINESS_ACTION_NAME = "admin_create_business";
+    public static final String ADMIN_TOGGLE_RESTAURANT_ACTION_NAME = "admin_toggle_restaurant_feature";
+    public static final String ADMIN_CREATE_OFFICE_ACTION_NAME = "admin_create_office";
+    public static final String ADMIN_DELETE_OFFICE_ACTION_NAME = "admin_delete_office";
+    public static final String ADMIN_CREATE_CASH_REGISTER_ACTION_NAME = "admin_create_cash_register";
+    public static final String ADMIN_EDIT_CASH_REGISTER_ACTION_NAME = "admin_edit_cash_register";
+    public static final String ADMIN_DELETE_CASH_REGISTER_ACTION_NAME = "admin_delete_cash_register";
+    public static final String SET_MANAGER_ACTION_NAME = "set_manager";
+    public static final String ASSIGN_EMPLOYEE_FOR_OFFICE_ACTION_NAME = "assign_employee_for_office";
+    public static final String UNASSIGN_EMPLOYEE_FOR_OFFICE_ACTION_NAME = "unassign_employee_for_office";
+    public static final String SET_MAIN_OFFICE_ACTION_NAME = "set_main_office";
+
+    public static final String FIRE_EMPLOYEE_ACTION_NAME = "fire_employee";
+
+    public static final String ADD_NOTIFICATION_ACTION_NAME = "add_notification";
+    public static final String MARK_NOTIFICATION_ACTION_NAME = "mark_notification";
+    public static final String DELETE_NOTIFICATION_ACTION_NAME = "delete_notification";
+    public static final String NOTIFY_ADMIN_OPEN_OFFICE_ACTION_NAME = "notify_admin_open_office";
+    public static final String NOTIFY_ADMIN_CLOSE_OFFICE_ACTION_NAME = "notify_admin_close_office";
+    public static final String ADMIN_READ_NOTIFICATION_ACTION_NAME = "admin_read_notification";
+
+    public static final String RESET_PASSWORD_ACTION_NAME = "reset_password";
+    public static final String SAVE_PASSWORD_ACTION_NAME = "save_password";
+
+    public static final String CREATE_PRODUCT_ACTION_NAME = "create_product";
+    public static final String UPLOAD_IMAGE_ACTION_NAME = "upload_image";
+    public static final String UPDATE_PRODUCT_ACTION_NAME = "update_product";
+    public static final String DELETE_PRODUCT_ACTION_NAME = "delete_product";
+    public static final String PRODUCTS_TO_OFFICE_ACTION_NAME = "products_to_office";
+    public static final String ADD_COMMENT_ON_PRODUCT_ACTION_NAME = "add_comment_on_product";
+    public static final String DELETE_COMMENT_ON_PRODUCT_ACTION_NAME = "delete_comment_on_product";
+    public static final String UPDATE_DISCOUNT_ACTION_NAME = "update_discount";
+
+    public static final String SAVE_QUESTION_ACTION_NAME = "save_question";
+    public static final String DELETE_QUESTION_ACTION_NAME = "delete_question";
+    public static final String SAVE_ANSWER_ACTION_NAME = "save_answer";
+    public static final String DELETE_ANSWER_ACTION_NAME = "delete_answer";
+
+    public static final String SAVE_RECEIPT_ACTION_NAME = "save_receipt";
+    public static final String UPDATE_RECEIPT_STATUS_ACTION_NAME = "update_receipt_status";
+
+    public static final String UPDATE_USER_PROFILE_ACTION_NAME = "update_user_profile";
+    public static final String UPDATE_USER_ROLES_ACTION_NAME = "update_user_roles";
+
+    public static final String REGISTER_INCOMING_PRODUCTS_ACTION_NAME = "register_incoming_products";
+
 }
--- src/main/java/ba/unsa/etf/si/mainserver/controllers/admin/LogController.java
+++ src/main/java/ba/unsa/etf/si/mainserver/controllers/admin/LogController.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.si.mainserver.controllers.admin;
 
+import ba.unsa.etf.si.mainserver.configurations.Actions;
+import ba.unsa.etf.si.mainserver.responses.admin.logs.ActionCollectionResponse;
 import ba.unsa.etf.si.mainserver.responses.admin.logs.LogCollectionResponse;
 import ba.unsa.etf.si.mainserver.services.admin.logs.LogServerService;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
@@ -28,5 +33,54 @@ public class LogController {
             @RequestParam(name = "object",required = false) String object
     ) {
         return ResponseEntity.ok(logServerService.getLogsFromServer(username,from,to,action,object));
+    }
+
+    @GetMapping("/logs/actions")
+    @Secured("ROLE_ADMIN")
+    public ResponseEntity<ActionCollectionResponse> getActions() {
+        return ResponseEntity.ok(new ActionCollectionResponse(Arrays.asList(
+                Actions.LOGIN_ACTION_NAME,
+                Actions.PASSWORD_CHANGE_ACTION_NAME,
+                Actions.ADMIN_PASSWORD_CHANGE_ACTION_NAME,
+                Actions.ADMIN_REGISTER_USER_ACTION_NAME,
+                Actions.REGISTER_USER_ACTION_NAME,
+                Actions.ADMIN_CREATE_BUSINESS_ACTION_NAME,
+                Actions.ADMIN_TOGGLE_RESTAURANT_ACTION_NAME,
+                Actions.ADMIN_CREATE_OFFICE_ACTION_NAME,
+                Actions.ADMIN_DELETE_OFFICE_ACTION_NAME,
+                Actions.ADMIN_CREATE_CASH_REGISTER_ACTION_NAME,
+                Actions.ADMIN_EDIT_CASH_REGISTER_ACTION_NAME,
+                Actions.ADMIN_DELETE_CASH_REGISTER_ACTION_NAME,
+                Actions.SET_MANAGER_ACTION_NAME,
+                Actions.ASSIGN_EMPLOYEE_FOR_OFFICE_ACTION_NAME,
+                Actions.UNASSIGN_EMPLOYEE_FOR_OFFICE_ACTION_NAME,
+                Actions.SET_MAIN_OFFICE_ACTION_NAME,
+                Actions.FIRE_EMPLOYEE_ACTION_NAME,
+                Actions.ADD_NOTIFICATION_ACTION_NAME,
+                Actions.MARK_NOTIFICATION_ACTION_NAME,
+                Actions.DELETE_NOTIFICATION_ACTION_NAME,
+                Actions.NOTIFY_ADMIN_OPEN_OFFICE_ACTION_NAME,
+                Actions.NOTIFY_ADMIN_CLOSE_OFFICE_ACTION_NAME,
+                Actions.ADMIN_READ_NOTIFICATION_ACTION_NAME,
+                Actions.RESET_PASSWORD_ACTION_NAME,
+                Actions.SAVE_PASSWORD_ACTION_NAME,
+                Actions.CREATE_PRODUCT_ACTION_NAME,
+                Actions.UPLOAD_IMAGE_ACTION_NAME,
+                Actions.UPDATE_PRODUCT_ACTION_NAME,
+                Actions.DELETE_PRODUCT_ACTION_NAME,
+                Actions.PRODUCTS_TO_OFFICE_ACTION_NAME,
+                Actions.ADD_COMMENT_ON_PRODUCT_ACTION_NAME,
+                Actions.DELETE_COMMENT_ON_PRODUCT_ACTION_NAME,
+                Actions.UPDATE_DISCOUNT_ACTION_NAME,
+                Actions.SAVE_QUESTION_ACTION_NAME,
+                Actions.DELETE_QUESTION_ACTION_NAME,
+                Actions.SAVE_ANSWER_ACTION_NAME,
+                Actions.DELETE_ANSWER_ACTION_NAME,
+                Actions.SAVE_RECEIPT_ACTION_NAME,
+                Actions.UPDATE_RECEIPT_STATUS_ACTION_NAME,
+                Actions.UPDATE_USER_PROFILE_ACTION_NAME,
+                Actions.UPDATE_USER_ROLES_ACTION_NAME,
+                Actions.REGISTER_INCOMING_PRODUCTS_ACTION_NAME
+        )));
     }
 }

e8c6e92f7c430c6adcc4444d3b85b6f24982f45b
Azra Balić <abalic2@etf.unsa.ba>
2020-04-28 22:47:11 +0200

    Added 17% rate

--- src/main/resources/db/migration/V12__pdv.sql
+++ src/main/resources/db/migration/V12__pdv.sql
@@ -3,3 +3,13 @@ alter table receipt_items add column pdv float;
 
 update products set pdv = 17;
 update receipt_items set pdv = 17;
+
+create table pdv_rates
+(
+    id       bigint           not null
+        constraint pdv_rates_pkey
+            primary key,
+    pdv_rate double precision not null
+);
+
+INSERT INTO public.pdv_rates (id, pdv_rate) VALUES (94, 17);

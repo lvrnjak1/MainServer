9a5e077bfc0b61555259b5fa9556517f0b573843
Azra Balić <abalic2@etf.unsa.ba>
2020-04-30 14:51:04 +0200

    Added max number constraints

--- src/main/java/ba/unsa/etf/si/mainserver/services/business/BusinessService.java
+++ src/main/java/ba/unsa/etf/si/mainserver/services/business/BusinessService.java
@@ -102,6 +102,10 @@ public class BusinessService {
         }
     }
 
+    public int countOfficesInBusiness(Long businessId){
+        return officeRepository.findAllByBusinessId(businessId).size();
+    }
+
 //    public Optional<Business> getBusinessByProductId(Product product){
 //        Optional<Office> officeOptional = officeRepository.findById(product.getId());
 //        if(officeOptional.isPresent()){
